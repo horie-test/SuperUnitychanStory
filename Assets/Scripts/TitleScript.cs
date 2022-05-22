b398946b7d49502f394b6c3b05b635b94081c814
horie-private <sawazas00@gmail.com>
2022-05-23 00:01:03 +0900

    タイトル追加

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // クリックした時
+        if (Input.GetMouseButtonDown(0))
+        {
+            // シーン遷移
+            SceneManager.LoadScene("FirstScene");
+        }
+    }
+}
