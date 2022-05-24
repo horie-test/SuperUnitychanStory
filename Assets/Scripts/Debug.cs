1faaeb3f3d6438ea4f227e259990fb8975e57b74
horie-private <sawazas00@gmail.com>
2022-05-25 07:15:16 +0900

    木、生やしてみた

---
+++ Assets/Scripts/Debug.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugFunc : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 同時押しで初期位置へリスポーン
+        if (Input.GetMouseButtonDown(0))
+        {
+            player.transform.position = new Vector3(0, 2, 0);
+        }
+        
+    }
+}
