a391b666823ffe5ec2c0a578d4970dd89b85994c
horie-private <sawazas00@gmail.com>
2022-05-25 01:21:15 +0900

    移動スクリプトの共通化

--- Assets/Scripts/GameObject/MoveHorizontalScript.cs
+++ Assets/Scripts/GameObject/MoveHorizontalScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MoveHorizontalScript : MonoBehaviour
 {
+    // 移動方向（x,y,z）いずれかをUIから指定
+    public string dim = "";
+    // 移動区間をUIから指定
+    public int interval = 0;
     int counter = 0;
-    float move = 0.1f;
+    float move = 0.1f; // 移動フレームは固定
+    Vector3 position;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +21,27 @@ public class MoveHorizontalScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = new Vector3(0, 0, move);
+        switch (dim)
+        {
+            case "x":
+                position = new Vector3(move, 0, 0);
+                break;
+            case "y":
+                position = new Vector3(0, move, 0);
+                break;
+            case "z":
+                position = new Vector3(0, 0, move);
+                break;
+            default:
+                position = new Vector3(move, 0, 0);
+                break;
+
+        }
+        
         transform.Translate(position);
         counter++;
 
-        if (counter == 300)
+        if (counter == interval)
         {
             counter = 0;
             move *= -1;
